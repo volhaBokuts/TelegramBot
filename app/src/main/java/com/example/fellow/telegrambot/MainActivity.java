0df79fe330b57c0f9c6b2a230cfeb6d8c3130765
Fellow <bokut.olya@gmail.com>
2016-07-12 14:51:34 +0300

    add gson library

--- app/src/main/java/com/example/fellow/telegrambot/MainActivity.java
+++ app/src/main/java/com/example/fellow/telegrambot/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.fellow.telegrambot;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final AsyncTask<String, Void, String> asyncTask = new AsyncTask<String, Void, String>() {
+            @Override
+            protected String doInBackground(String... params) {
+                String json = null;
+                try {
+                    TelegramClient telegramClient = new TelegramClient();
+                    json = telegramClient.getJson(GET_UPDATES_URL);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return json;
+            }
+        };
+
     }
 }
