f06a5f996bd46c2e78e98316692dbe2efd31ef88
nikitaart <nikitaart2805@gmail.com>
2016-07-21 21:11:16 +0300

    add getPhotos, getFile, add changes

--- app/src/main/java/com/example/fellow/telegrambot/dto/getphotos/Photos.java
+++ app/src/main/java/com/example/fellow/telegrambot/dto/getphotos/Photos.java
@@ -6,4 +6,15 @@ public class Photos {
     private String file_size;
     private String file_path;
 
+    public String getFile_id() {
+        return file_id;
+    }
+
+    public String getFile_size() {
+        return file_size;
+    }
+
+    public String getFile_path() {
+        return file_path;
+    }
 }
